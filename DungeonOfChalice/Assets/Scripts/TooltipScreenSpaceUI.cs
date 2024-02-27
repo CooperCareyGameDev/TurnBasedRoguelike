c0e56206e16cdd5f799c25b2050e8163575be69e
Cooper Carey <65479428+CooperCareyGameDev@users.noreply.github.com>
2024-02-27 09:59:27 -0800

    Tooltips

---
+++ DungeonOfChalice/Assets/Scripts/TooltipScreenSpaceUI.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro; 
+
+public class TooltipScreenSpaceUI : MonoBehaviour
+{
+    public static TooltipScreenSpaceUI Instance { get; private set; }
+
+
+
+    [SerializeField] private RectTransform canvasRectTransform; 
+    private RectTransform backgroundRectTransform;
+    private TextMeshProUGUI textMeshPro;
+    private RectTransform rectTransform;
+
+    private System.Func<string> getTooltipTextFunc;
+    private void Awake()
+    {
+        Instance = this; 
+        backgroundRectTransform = transform.Find("Background").GetComponent<RectTransform>();
+        textMeshPro = transform.Find("TooltipText").GetComponent<TextMeshProUGUI>();
+        rectTransform = transform.GetComponent<RectTransform>();
+        SetText("Placeholder the image should change to fit the text now");
+
+        HideTooltip();
+    }
+
+    private void SetText(string tooltipText)
+    {
+        textMeshPro.SetText(tooltipText);
+        textMeshPro.ForceMeshUpdate();
+        Vector2 textSize = textMeshPro.GetRenderedValues(false);
+        Vector2 paddingSize = new Vector2(8, 8);
+        backgroundRectTransform.sizeDelta = textSize + paddingSize;
+    }
+
+    private void Update()
+    {
+        SetText(getTooltipTextFunc);
+
+        Vector2 anchoredPosition = Input.mousePosition / canvasRectTransform.localScale.x;
+
+        if (anchoredPosition.x + backgroundRectTransform.rect.width > canvasRectTransform.rect.width)
+        {
+            anchoredPosition.x = canvasRectTransform.rect.width - backgroundRectTransform.rect.width;
+        }
+        if (anchoredPosition.y + backgroundRectTransform.rect.height > canvasRectTransform.rect.height)
+        {
+            anchoredPosition.y = canvasRectTransform.rect.height - backgroundRectTransform.rect.height;
+        }
+        rectTransform.anchoredPosition = anchoredPosition;
+    }
+
+    private void ShowTooltip(string tooltipText)
+    {
+        gameObject.SetActive(true);
+        SetText(tooltipText);
+    }
+
+    private void HideTooltip()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public static void ShowTooltip_Static(string tooltipText)
+    {
+        Instance.ShowTooltip(tooltipText);
+    }
+
+    private void ShowTooltip(System.Func<string> getTooltipTextFunc)
+    {
+        this.getTooltipTextFunc = getTooltipTextFunc;
+        gameObject.SetActive(true);
+        SetText(getTooltipTextFunc);
+    }
+    public static void ShowTooltip_Static(System.Func<string> getTooltipTextFunc)
+    {
+        Instance.ShowTooltip(getTooltipTextFunc);
+    }
+    public static void HideTooltip_Static()
+    {
+        Instance.HideTooltip();
+    }
+}
