19ff984b8f9f2bee212a3daa0fdcc31731fa5d50
Cooper Carey <65479428+CooperCareyGameDev@users.noreply.github.com>
2024-04-23 09:01:53 -0700

    enemy prefabs

--- DungeonOfChalice/Assets/Scripts/EnemySpawner.cs
+++ DungeonOfChalice/Assets/Scripts/EnemySpawner.cs
@@ -33,19 +33,133 @@ public class EnemySpawner : MonoBehaviour
 
     public List<Transform> spawnLocations = new List<Transform>();
 
+    BattleHandler battleHandler;
+    private bool once2 = true, once3 = true, once4 = true, once5 = true, once6 = true, once7 = true, once8 = true, once9 = true, once10 = true, once11 = true, once12 = true;
     private void Awake()
     {
-        Instantiate(smallMushroomEnemy, spawnLocations[0].position, Quaternion.identity);
+        int randomNo2 = Random.Range(1, 4), randomNo3 = Random.Range(1, 4), randomNo4 = Random.Range(1, 4), randomNo5 = Random.Range(1, 4), randomNo6 = Random.Range(1, 4),
+        randomNo7 = Random.Range(1, 4), randomNo8 = Random.Range(1, 4), randomNo9 = Random.Range(1, 4), randomNo10 = Random.Range(1, 4), randomNo11 = Random.Range(1, 4), randomNo12 = Random.Range(1, 4);
+    battleHandler = FindFirstObjectByType<BattleHandler>();
+        //Instantiate(smallMushroomEnemy, spawnLocations[0].position, Quaternion.identity);
         Instantiate(smallMushroomEnemy, spawnLocations[1].position, Quaternion.identity);
         Instantiate(smallMushroomEnemy, spawnLocations[2].position, Quaternion.identity);
         Instantiate(smallMushroomEnemy, spawnLocations[3].position, Quaternion.identity);
-        Instantiate(smallMushroomEnemy, spawnLocations[4].position, Quaternion.identity);
+        //Instantiate(smallMushroomEnemy, spawnLocations[4].position, Quaternion.identity);
     }
 
     private void Update()
     {
         waveText.text = $"Current Wave: {currentWave}";
         //Debug.Log(Random.Range(1, 4));
+        
+        if (currentWave == 2 && once2)
+        {
+
+            foreach (GameObject player in battleHandler.players)
+            {
+                player.GetComponent<CharacterBattle>().hasDoneTurn = false;
+            }
+            Instantiate(smallMushroomEnemy, spawnLocations[1].position, Quaternion.identity);
+            Instantiate(goldVineEnemy, spawnLocations[2].position, Quaternion.identity);
+            Instantiate(goldVineEnemy, spawnLocations[3].position, Quaternion.identity);
+            Instantiate(smallMushroomEnemy, spawnLocations[4].position , Quaternion.identity);
+            once2 = false;
+            GameObject[] enemyArray = GameObject.FindGameObjectsWithTag("Enemy");
+            foreach (GameObject enemy in enemyArray)
+            {
+                battleHandler.enemies.Add(enemy);
+            }
+        }
+        else if (currentWave == 3)
+        {
+
+            GameObject[] enemyArray = GameObject.FindGameObjectsWithTag("Enemy");
+            foreach (GameObject enemy in enemyArray)
+            {
+                battleHandler.enemies.Add(enemy);
+            }
+        }
+        else if (currentWave == 4)
+        {
+
+            GameObject[] enemyArray = GameObject.FindGameObjectsWithTag("Enemy");
+            foreach (GameObject enemy in enemyArray)
+            {
+                battleHandler.enemies.Add(enemy);
+            }
+        }
+        else if (currentWave == 5)
+        {
+
+            GameObject[] enemyArray = GameObject.FindGameObjectsWithTag("Enemy");
+            foreach (GameObject enemy in enemyArray)
+            {
+                battleHandler.enemies.Add(enemy);
+            }
+        }
+        else if (currentWave == 6)
+        {
+
+            GameObject[] enemyArray = GameObject.FindGameObjectsWithTag("Enemy");
+            foreach (GameObject enemy in enemyArray)
+            {
+                battleHandler.enemies.Add(enemy);
+            }
+        }
+        else if (currentWave == 7)
+        {
+
+            GameObject[] enemyArray = GameObject.FindGameObjectsWithTag("Enemy");
+            foreach (GameObject enemy in enemyArray)
+            {
+                battleHandler.enemies.Add(enemy);
+            }
+        }
+        else if (currentWave == 8)
+        {
+
+            GameObject[] enemyArray = GameObject.FindGameObjectsWithTag("Enemy");
+            foreach (GameObject enemy in enemyArray)
+            {
+                battleHandler.enemies.Add(enemy);
+            }
+        }
+        else if (currentWave == 9)
+        {
+
+            GameObject[] enemyArray = GameObject.FindGameObjectsWithTag("Enemy");
+            foreach (GameObject enemy in enemyArray)
+            {
+                battleHandler.enemies.Add(enemy);
+            }
+        }
+        else if (currentWave == 10)
+        {
+
+            GameObject[] enemyArray = GameObject.FindGameObjectsWithTag("Enemy");
+            foreach (GameObject enemy in enemyArray)
+            {
+                battleHandler.enemies.Add(enemy);
+            }
+        }
+        else if (currentWave == 11)
+        {
+
+            GameObject[] enemyArray = GameObject.FindGameObjectsWithTag("Enemy");
+            foreach (GameObject enemy in enemyArray)
+            {
+                battleHandler.enemies.Add(enemy);
+            }
+        }
+        else if (currentWave == 12)
+        {
+
+            GameObject[] enemyArray = GameObject.FindGameObjectsWithTag("Enemy");
+            foreach (GameObject enemy in enemyArray)
+            {
+                battleHandler.enemies.Add(enemy);
+            }
+        }
     }
 
     public void IncrementWave()
