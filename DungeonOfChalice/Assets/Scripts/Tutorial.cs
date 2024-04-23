43611deeac85a29a4a92bf30f3b6031351882465
Cooper Carey <65479428+CooperCareyGameDev@users.noreply.github.com>
2024-04-23 10:23:21 -0700

    Updated tutorial

--- DungeonOfChalice/Assets/Scripts/Tutorial.cs
+++ DungeonOfChalice/Assets/Scripts/Tutorial.cs
@@ -2,10 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 public class Tutorial : MonoBehaviour
 {
+    [SerializeField] TextMeshProUGUI tutorialText;
+    [TextArea]
+    [SerializeField] string attackingText;
+    [TextArea]
+    [SerializeField] string rageText;
+    [TextArea]
+    [SerializeField] string statusEffectsText; 
     public void LoadMainMenu()
     {
         SceneManager.LoadScene("MainMenu");
+    }
+
+    public void SetAttackingText()
+    {
+        tutorialText.text = attackingText;
+    }
+
+    public void SetRageText()
+    {
+        tutorialText.text = rageText;
+    }
+
+    public void SetStatusEffectsText()
+    {
+        tutorialText.text = statusEffectsText;
     }
 }
